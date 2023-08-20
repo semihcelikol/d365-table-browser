f3d8583326998dd506900ac3baec004ef0959ef6
Semih Çelikol <semihcelikol@outlook.com>
2023-08-20 16:58:35 +0300

    update

--- firefox/mainHelper.js
+++ firefox/mainHelper.js
@@ -1,34 +1,55 @@
-document.addEventListener("click", (e) => {
-    
-    var baseUrl = null;
-    var tableName = null;
-    var companyCode = null;
 
-    if(e.target.name == "btnShow")
-    {
-        baseUrl = document.getElementById("baseUrl").value;
-        tableName = document.getElementById("tableName").value;
-        companyCode = document.getElementById("companyCode").value;
+window.onload = function() {
+    const btnGithub = document.getElementById("btnGithub");
+    const btnBuymeacoffee = document.getElementById("btnBuymeacoffee");
+    const btnShow = document.getElementById("btnShow");
 
-        var ret = baseUrl.search(".com/");
+    if(btnGithub != null) {
+        btnGithub.onclick = function() {
+            openLink("https://github.com/semihcelikol/d365-table-browser");
+        };
+    }
 
-        if((baseUrl != "")
+    if(btnBuymeacoffee != null) {
+        btnBuymeacoffee.onclick = function() {
+            openLink("https://www.buymeacoffee.com/semihc");
+        };
+    }
+
+    if(btnShow != null) {
+        btnShow.onclick = function() {
+            openTableBrowserLink();
+        };
+    }
+}
+
+function openTableBrowserLink() {
+    var baseUrl = document.getElementById("baseUrl").value;
+    var tableName = document.getElementById("tableName").value;
+    var companyCode = document.getElementById("companyCode").value;
+
+    var ret = baseUrl.search(".com/");
+
+    if((baseUrl != "")
         && tableName != ""
         && companyCode != "")
         {
             if(ret > 0)
             {
-                window.open(baseUrl + "?mi=SysTableBrowser&TableName="+tableName+"&cmp="+companyCode, "_blank");
+                openLink(baseUrl + "?mi=SysTableBrowser&TableName="+tableName+"&cmp="+companyCode);
             }
             else
             {
                 baseUrl += "/";
-                window.open(baseUrl + "?mi=SysTableBrowser&TableName="+tableName+"&cmp="+companyCode, "_blank");
+                openLink(baseUrl + "?mi=SysTableBrowser&TableName="+tableName+"&cmp="+companyCode);
             }
         }
         else
         {
             alert("Tüm alanları doldurmalısınız.");
         }
-    }
-})
+};
+
+function openLink(url){
+    window.open(url, "_blank");
+}
